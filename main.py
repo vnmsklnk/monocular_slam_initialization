9a0f51350b5279cf2845c1946e698d3d585f8f90
Ivan Moskalenko <vnmsklnk@yandex.ru>
2024-11-18 23:56:34 +0300

    code style reformat

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ matcher = msi.features.FeatureMatcherTypes.XFEAT
 # matcher = msi.features.FeatureMatcherTypes.LIGHTGLUE
 
 feature_manager = msi.features.FeatureManager(num_features=2000, detector_type=detector)
-feature_matcher = msi.features.feature_matcher_factory(matcher_type=matcher, detector_type=detector)
-num_fin_features = 150 # minimum features for successful initialization
+feature_matcher = msi.features.feature_matcher_factory(
+    matcher_type=matcher, detector_type=detector
+)
+num_fin_features = 150  # minimum features for successful initialization
 use_robust_kernel = True
 
 # Uncomment for ICL
@@ -24,21 +26,21 @@ use_robust_kernel = True
 # cur_img = cv2.cvtColor(cv2.imread('rgb_icl/47.png'), cv2.COLOR_BGR2RGB)
 
 # Params for TUM RGB-D fr3
-K = np.array([[535.4, 0, 320.1],
-             [0, 539.2, 247.6],
-             [0, 0, 1]])
-ref_img = cv2.cvtColor(cv2.imread('rgb_tum/1341847980.722988.png'), cv2.COLOR_BGR2RGB)
-cur_img = cv2.cvtColor(cv2.imread('rgb_tum/1341847981.994808.png'), cv2.COLOR_BGR2RGB)
+K = np.array([[535.4, 0, 320.1], [0, 539.2, 247.6], [0, 0, 1]])
+ref_img = cv2.cvtColor(cv2.imread("rgb_tum/1341847980.722988.png"), cv2.COLOR_BGR2RGB)
+cur_img = cv2.cvtColor(cv2.imread("rgb_tum/1341847981.994808.png"), cv2.COLOR_BGR2RGB)
 
-D = np.array([0, 0, 0, 0, 0]) # ICL and TUM (fr3) are undistorted
+D = np.array([0, 0, 0, 0, 0])  # ICL and TUM (fr3) are undistorted
 
-initializer = msi.Initializer(num_fin_features, use_robust_kernel, feature_manager, feature_matcher)
+initializer = msi.Initializer(
+    num_fin_features, use_robust_kernel, feature_manager, feature_matcher
+)
 reference = msi.Frame(ref_img, feature_manager, K, D)
 current = msi.Frame(cur_img, feature_manager, K, D)
 points = initializer.initialize(reference, current)
 
 if len(points) < num_fin_features:
-    print('Not enough points for initialization!')
+    print("Not enough points for initialization!")
     exit()
 
 positions = []
